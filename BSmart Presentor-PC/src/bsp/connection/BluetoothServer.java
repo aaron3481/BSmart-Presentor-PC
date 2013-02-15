b7e4577bff47b23233f58f3d61ab7e051e7859f1
Jiannan Cai <aaron3481.school@gmail.com>
2013-02-15 14:33:40 -0500

    N/A

--- BSmart Presentor-PC/src/bsp/connection/BluetoothServer.java
+++ BSmart Presentor-PC/src/bsp/connection/BluetoothServer.java
@@ -13,13 +13,59 @@ import javax.microedition.io.StreamConnection;
 import javax.microedition.io.StreamConnectionNotifier;
 
 public class BluetoothServer {
-	private final static UUID SERVER_UUID = new UUID("CAD00C83466FFFDC194DCCAB3816FFBE",false);
-	
+	private final static UUID SERVER_UUID = new UUID("00001101CADFFFDC194DCCAB3816FFBE",false);
+	private final String URL;
 	private StreamConnectionNotifier notifier;
-	
 	private LocalDevice localDev = null;
+	private ServiceRecord serviceRecord;
+	private final String paringCode;
+	private String currentDeviceMAC;
+	private boolean isOpen;
 	
-	public static void main(String[]args){
-		System.out.println(SERVER_UUID.toString());
+	
+	
+	public BluetoothServer(){
+		paringCode = generateCode();
+		currentDeviceMAC = null;
+		isOpen = false;
+		
+		URL = "btspp://localhost:" +
+				SERVER_UUID.toString() +
+				";name=BSPServer" +
+				";authorize=false";
 	}
+	
+	public String getParingCode() {
+		return paringCode;
+	}
+	
+	private String generateCode(){
+		java.util.Random ra = new java.util.Random(1358);
+		int temp=0;
+		for(int i=0;i<6;i++){
+			temp+=(ra.nextInt(9)+1)*10^i;
+		}
+		
+		return String.valueOf(temp);
+	}
+	
+	
+	class ServerThread implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO this is the first version of the server
+			// and may need to add some thread-safe components here
+			try{
+				notifier=(StreamConnectionNotifier)Connector.open(URL);
+				serviceRecord = localDev.getRecord(notifier);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+			
+		}
+		
+	}
+
 }
